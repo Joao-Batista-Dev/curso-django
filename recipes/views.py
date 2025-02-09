0be04325d77a39bf5c9a411ac5e47689fd0d4f83
Joao Batista <joaobldss@gmail.com>
2025-02-09 06:35:30 -0300

    Criando nova rotas

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render # Importando o render - renderizar o nossos arquivos HTML
+from django.shortcuts import render, get_list_or_404 # Importando o render - get_list_or_404 para error 
 from django.http import HttpResponse # importando o Protocolo HTTP
 from utils.recipes.factory import make_recipe # Importando pacote de dados FAKER
-from recipes.models import Recipe # Importando o models RECIPE
+from .models import Recipe # Importando o models RECIPEencontrar retorna o error 404
+
 
 def home(request):
     recipes = Recipe.objects.filter(
@@ -20,27 +21,34 @@ def home(request):
 
 # Criando a views de categoria
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
+    # Pegando dados e retornando error 404 se pagina não for encontrada
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
         category__id=category_id,
-        is_published = True,  # Aplicando filtro na minhas publicacao
-        ).order_by(
-            '-id',
-            ) # Pegando os dados do nosso models e ordenando em ordem descrecente
-    
+        is_published = True, 
+        ).order_by('-id',), 
+    )
+
     return render(
         request, 
         'recipes/pages/home.html', # renderizar arquivo HTML
         context={ # passando um contexto para exibir no html = dicionario
             'recipes': recipes, # Para ixibir os dados para o usuário
+            'title': f'{recipes[0].category.name} - Category' # Filtrando url pelo name
         }
     )
 
 def recipe(request, id): # Criando minha views dinâmica
+    recipe = Recipe.objects.filter(
+        pk=id,
+        is_published = True,
+    ).order_by('-id').first()
+
     return render(
         request, 
         'recipes/pages/recipe-views.html', # renderizar arquivo HTML
         context={ # passando um contexto para exibir no html = dicionario
-            'recipe': make_recipe(), # chamando os dados da minha aplicacao
+            'recipe': recipe, # chamando os dados da minha aplicacao
             'is_detail_page': True, # Ocuntando minha lista de detalhes
         }
     )
