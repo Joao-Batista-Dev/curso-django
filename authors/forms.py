b77e8d0c562e61019034c5fef7afd1f3477cee44
Joao Batista <joaobldss@gmail.com>
2025-04-17 10:49:10 -0300

    Utilizando o atributo validators do próprio campo para validação de dados

--- authors/forms.py
+++ authors/forms.py
@@ -2,6 +2,23 @@
 from django import forms # import forms
 from django.contrib.auth.models import User # import models Users
 from django.core.exceptions import ValidationError # import ValidationError
+import re # importando minha regex
+
+# funcao para validar determinado campo
+def strong_password(password):
+    # verificar se minha senha e: A - Z: a-z: 0-9: 8 caracteres
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+
+    # verificar se a regez da meth com a minha estring que o password 
+    if not regex.match(password):
+        # levantando o error
+        raise ValidationError((
+             'Password must have at least one uppercase letter, '
+             'one lowercase letter and one number. The length should be '
+             'at least 8 characters.'
+         ),
+             code='invalid'
+         )
 
 # criando forms atrelado ao models existente
 class RegisterForm(forms.ModelForm):
@@ -39,6 +56,8 @@ class RegisterForm(forms.ModelForm):
         widget=forms.PasswordInput(attrs={
             'placeholder': 'Your password'
         }),
+        # usando o meu validador de senha com regex
+        validators=[strong_password]
     )
 
     password2 = forms.CharField(
