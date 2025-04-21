63b49b90f0e358fd70c13209a6c5f8a7eef877b2
Joao Batista <joaobldss@gmail.com>
2025-04-21 20:49:13 -0300

    Testando label com teste unitário

--- authors/tests/test_author_register_form.py
+++ authors/tests/test_author_register_form.py
@@ -1,25 +1,27 @@
 # Importa a classe base de teste do Django.
 from django.test import TestCase
-# Importa o formulário que será testado. Esse formulário está em authors/forms.py.
-from authors.forms import RegisterForm
-# Importa o decorador @parameterized.expand que 
-# permite testar o mesmo método com diferentes
-# valores de entrada, evitando repetição de código.
-from parameterized import parameterized
-
-# Cria uma classe de teste que herda de TestCase do Django
-# o que permite rodar testes com banco de
-# dados temporário e integração com o sistema de testes do Django.
 '''
+Importa o formulário que será testado. 
+Esse formulário está em authors/forms.py.
+'''
+from authors.forms import RegisterForm
+'''
+Importa o decorador @parameterized.expand que 
+permite testar o mesmo método com diferentes
+valores de entrada, evitando repetição de código.
+'''
+from parameterized import parameterized
+'''
+Cria uma classe de teste que herda de TestCase do Django
+o que permite rodar testes com banco de
+dados temporário e integração com o sistema de testes do Django.
+
 Esse decorador executa o método test_first_name_placeholder_is_correct 
 várias vezes, uma para cada par (field, placeholder) na lista.
 
 Por exemplo:
-
 Primeira execução: field = 'username', placeholder = 'Your username'
-
 Segunda execução: field = 'email', placeholder = 'Your e-mail'
-
 E assim por diante...
 '''
 
@@ -32,7 +34,7 @@ class AuthorRegisterFormUnitTest(TestCase):
         ('password', 'Type your password'),
         ('password2', 'Repeat your password'),
     ])
-    def test_first_name_placeholder_is_correct(self, field, placeholder):
+    def test_fields_placeholder(self, field, placeholder):
         form = RegisterForm()  # Cria uma instância do formulário
         current_placeholder = form[field].field.widget.attrs['placeholder']  # Pega o placeholder do campo
         self.assertEqual(current_placeholder, placeholder)  # Verifica se o valor está correto
