f87de4e0f731909fec59f4ca55356c85fc209d45
Joao Batista <joaobldss@gmail.com>
2025-05-23 11:09:11 -0300

    Search ListView

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,7 +5,7 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', views.RecipeListViewsHome.as_view(), name="home"),
-    path('recipes/search/', views.search, name="search"),
+    path('recipes/search/', views.RecipeListViewsSearch.as_view(), name="search"),
     path('recipes/category/<int:category_id>/',
          views.RecipeListViewsCategory.as_view(), name="category"),
     path('recipes/<int:id>/', views.recipe, name="recipe"),
--- recipes/views.py
+++ recipes/views.py
@@ -58,6 +58,37 @@ class RecipeListViewsCategory(RecipeListViewsBase):
         )
 
         return qs
+    
+class RecipeListViewsSearch(RecipeListViewsBase):
+    template_name = 'recipes/pages/category.html'
+
+    def get_queryset(self, *args, **kwargs):
+        search_term = self.request.GET.get('q', '').strip()
+        qs = super().get_queryset(*args, **kwargs)
+
+        qs = qs.filter(
+            Q(
+                Q(title__icontains=search_term) |
+                Q(description__icontains=search_term),
+            ),
+            is_published=True
+        )
+
+        return qs
+    
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+
+        search_term = self.request.GET.get('q', '').strip()
+
+        ctx.update({
+            'page_title': f'Search for "{search_term}" |',
+            'search_term': search_term,
+            'additional_url_query': f'&q={search_term}',
+        })
+
+        return ctx
+
 
 def category(request, category_id):
     recipes = get_list_or_404(
