12c50d61f2b06c736595b157f0723a9ae4c777a4
Joao Batista <joaobldss@gmail.com>
2025-05-23 10:44:15 -0300

    ListView: criando uma base para Home, Category e Search de recipes

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-
 from . import views
 
 app_name = 'recipes'
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', views.RecipeListViewsBase.as_view(), name="home"),
     path('recipes/search/', views.search, name="search"),
     path('recipes/category/<int:category_id>/',
          views.category, name="category"),
