ab9f5f342e57e9a77a7d6923444e178ea6baa41b
Joao Batista <joaobldss@gmail.com>
2025-05-28 20:24:53 -0300

    Corrigindo os testes da DetailView

--- recipes/views.py
+++ recipes/views.py
@@ -74,7 +74,7 @@ class RecipeListViewsCategory(RecipeListViewsBase):
 
         return ctx
     
-    
+
 class RecipeListViewsSearch(RecipeListViewsBase):
     template_name = 'recipes/pages/category.html'
 
@@ -114,6 +114,15 @@ class RecipeDetail(DetailView):
     model = Recipe
     context_object_name = 'recipe'
     template_name = 'recipes/pages/recipe-view.html'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+
+        qs = qs.filter(
+            is_published=True,
+        )
+
+        return qs
 
     def get_context_data(self, *args, **kwargs):
        ctx = super().get_context_data(*args, **kwargs)
