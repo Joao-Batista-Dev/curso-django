27dac0ea687ca848c230e6ae9eb397d6ad50e32a
Joao Batista <joaobldss@gmail.com>
2025-06-21 10:54:44 -0300

    Tag app: exibindo os dados em templates e criando uma view para tags

--- recipes/views.py
+++ recipes/views.py
@@ -8,6 +8,7 @@ from django.views.generic import ListView, DetailView
 from django.http import JsonResponse 
 from django.forms.models import model_to_dict
 from django.shortcuts import render
+from tag.models import Tag
 
 PER_PAGE = int(os.environ.get('PER_PAGE', 6))
 
@@ -181,3 +182,32 @@ class RecipeDetailApi(RecipeDetail):
             recipe_dict,
             safe=False
         )
+
+
+class RecipeListViewsTag(RecipeListViewsBase):
+    template_name = 'recipes/pages/tag.html'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+
+        qs = qs.filter(
+            tags__slug=self.kwargs.get('slug', '')
+        ).first()
+
+        return qs
+    
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+
+        page_title = Tag.objects.filter(slug=self.kwargs.get('slug', ''))
+
+        if not page_title:
+            page_title = 'No recipes found'
+
+        page_title = f'{page_title} - Tag |'
+
+        ctx.update({
+            'page_title': f'Search for "{page_title}" |',
+        })
+
+        return ctx
